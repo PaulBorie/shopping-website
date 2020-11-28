17c246335f89b5d1936e1eeb40f154f0ffaa359d
PaulBorie <paul.borie1@gmail.com>
2020-11-28 16:46:42 +0100

    minor fixes

--- deployment2.sh
+++ deployment2.sh
@@ -18,31 +18,3 @@ sed -i -e "s/d1/$FST_DOMAIN/g" -e "s/d2/$SND_DOMAIN/g" nginx-conf/nginx.conf
 sed -i -e "s/d1/$FST_DOMAIN/g" -e "s/d2/$SND_DOMAIN/g" -e "s/username/$USERNAME/g"  -e "s/mail_addr/$MAIL/g" docker-compose.yml
 
 docker-compose up -d    
-
-# Modifying the Web Server Configuration and Service Definition
-
-docker-compose stop webserver
-
-mkdir dhparam
-
-sudo openssl dhparam -out /home/"$USERNAME"/node_project/dhparam/dhparam-2048.pem 2048
-
-rm nginx-conf/nginx.conf
-
-#update nginx-conf
-
-sed -i -e "s/d1/$FST_DOMAIN/g" -e "s/d2/$SND_DOMAIN/g" nginx-conf/temp-nginx.conf
-
-mv nginx-conf/temp-nginx.conf nginx-conf/nginx.conf
-
-#update docker-compose.yml conf
-
-sed -i -e "s/d1/$FST_DOMAIN/g" -e "s/d2/$SND_DOMAIN/g" -e "s/username/$USERNAME/g"  -e "s/mail_addr/$MAIL/g" temp-docker-compose
-
-rm docker-compose.yml
-
-mv temp-docker-compose docker-compose.yml
-
-#Recreate the webserver service
-
-docker-compose up -d --force-recreate --no-deps webserver
