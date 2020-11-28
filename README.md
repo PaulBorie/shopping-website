5d69fb7b904518d1db58720db82711d899a7a2c2
PaulBorie <paul.borie1@gmail.com>
2020-11-28 14:17:11 +0100

    update the deployment.sh script

---
+++ README.md
@@ -0,0 +1,4 @@
+git add .
+git commit -m "minor bugs corrected"
+git push -u origin master
+ intall -y au docker .ce
--- deployment.sh
+++ deployment.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#initialize variable
+
 STRIPE_PUBLIC_KEY=$1
 STRIPE_PRIVATE_KEY=$2
 FST_DOMAIN=$3
@@ -7,13 +9,52 @@ SND_DOMAIN=$4
 USERNAME=$5
 MAIL=$6
 
+#assign the variables to the corresponding variables in the Dockerfile, nginx-conf and docker-compose.yml file
+
 sed -i -e "s/sk/$STRIPE_PUBLIC_KEY/g" -e "s/pk/$STRIPE_PRIVATE_KEY/g" Dockerfile
 
 sed -i -e "s/d1/$FST_DOMAIN/g" -e "s/d2/$SND_DOMAIN/g" nginx-conf/nginx.conf
 
 sed -i -e "s/d1/$FST_DOMAIN/g" -e "s/d2/$SND_DOMAIN/g" -e "s/username/$USERNAME/g"  -e "s/mail_addr/$MAIL/g" docker-compose.yml
 
-docker-compose up -d
+docker-compose up -d    
+
+# Modifying the Web Server Configuration and Service Definition
+
+docker-compose stop webserver
+
+mkdir dhparam
+
+sudo openssl dhparam -out /home/sammy/node_project/dhparam/dhparam-2048.pem 2048
+
+rm nginx-conf/nginx.conf
+
+#update nginx-conf
+
+sed -i -e "s/d1/$FST_DOMAIN/g" -e "s/d2/$SND_DOMAIN/g" nginx-conf/temp-nginx.conf
+
+mv nginx-conf/temp-nginx.conf nginx-conf/nginx.conf
+
+#update docker-compose.yml conf
+
+sed -i -e "s/d1/$FST_DOMAIN/g" -e "s/d2/$SND_DOMAIN/g" -e "s/username/$USERNAME/g"  -e "s/mail_addr/$MAIL/g" temp-docker-compose
+
+rm docker-compose.yml
+
+mv temp-docker-compose docker-compose.yml
+
+#Recreate the webserver service
+
+docker-compose up -d --force-recreate --no-deps webserver
+
+
+
+
+
+
+
+
+
 
 
 
