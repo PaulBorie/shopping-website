cfe1b0481f60e826efb0bccf713816bcee32832b
PaulBorie <paul.borie1@gmail.com>
2020-12-03 20:39:35 +0100

    minor bugs corrected

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install -y docker-ce 
