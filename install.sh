0e9cc339b56193d3049aeee2f7acad78026c521d
PaulBorie <paul.borie1@gmail.com>
2020-12-04 13:00:34 +0100

    minor bugs corrected

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "installation de docker"
+echo "installing docker..."
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -10,7 +10,7 @@ apt-cache policy docker-ce
 sudo apt install -y docker-ce 
 sleep 2
 
-echo "installation de docker-compose"
+echo "installing docker-compose..."
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
