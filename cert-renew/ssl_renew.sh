9508539673a2ac350252c0fe5adb544bca0fe21e
PaulBorie <paul.borie1@gmail.com>
2020-12-04 12:50:21 +0100

    minor bugs corrected

--- cert-renew/ssl_renew.sh
+++ cert-renew/ssl_renew.sh
@@ -4,5 +4,5 @@ COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
 cd /home/username/workdir/
-$COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
+$COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver > /var/log/certlog
 $DOCKER system prune -af
